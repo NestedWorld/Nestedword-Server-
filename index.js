93efe8fd7656f27ee2ac21dd5a7bc59e5260939f
Aude Lejuez <dusterherz@gmail.com>
2017-01-08 19:03:40 +0100

    caytait bidon D:

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+    socket.on('subscribe', function(room) {
+      console.log('joining room', room);
+      socket.join(room);
+  });
+
+  socket.on('send message', function(data) {
+      console.log('sending room post', data.room);
+      socket.broadcast.to(data.room).emit('conversation private post', {
+          message: data.message
+      });
+  });
+});
+
+http.listen(4241, function(){
+  console.log('listening on *:4241');
+});
